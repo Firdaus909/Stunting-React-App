beff9e034387f193cdab9efb77bc5538c6cdd038
Firdaus909 <muhammadzamzami109@gmail.com>
2022-07-07 22:14:03 +0700

    add unit testing

--- src/pages/__test__/Nutrition.test.tsx
+++ src/pages/__test__/Nutrition.test.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import { render } from './test-utils';
 import Nutrition from '../Nutrition';
+import userEvent from '@testing-library/user-event';
 
 test('Search button show successfully', () => {
   render(<Nutrition />);
   const searchBtn = screen.getByRole('button');
   expect(searchBtn).toBeInTheDocument();
+});
+
+test('Search input work successfully', async () => {
+  render(<Nutrition />);
+  const searchInput = screen.getByRole('textbox');
+
+  await userEvent.type(searchInput, 'Rice Bowl', { delay: 1 });
+  expect(searchInput).toHaveValue('Rice Bowl');
 });
